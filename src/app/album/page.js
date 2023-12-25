6830eaa04c20a4ffaa403159f098e92c35806900
Thales Neves <thales.neves@sap.ce.gov.br>
2023-12-25 15:41:34 -0300

    estilo fotos

--- src/app/album/page.js
+++ src/app/album/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Foto0 from '../../../public/assets/images/00.jpeg'
 import Foto1 from '../../../public/assets/images/01.jpeg'
 import Foto2 from '../../../public/assets/images/02.jpeg'
 import Foto3 from '../../../public/assets/images/03.jpeg'
@@ -13,44 +14,65 @@ export default function album() {
         <div className={styles.albumBox}>
 
             <Link href='/'>
-                <Button>Página inicial</Button>
+                <Button type="primary">Página inicial</Button>
             </Link>
 
             <div className={styles.albumFotoBox}>
 
+                <div className={styles.albumFotoBoxIndividual}>
+                    <span>A primeira foto ❤️</span>
+                    <Image
+                        src={Foto0}
+                        height='250'
+                        width='350'
+                        alt="Foto"
+                        className={styles.albumFoto}
+                    />
+                </div>
 
-                <Image
-                    src={Foto1}
-                    height='300'
-                    width='250'
-                    alt="Foto"
-                    className={styles.albumFoto}
-                />
+                <div className={styles.albumFotoBoxIndividual}>
+                    <span>Tinha que ser comendo 🥩</span>
+                    <Image
+                        src={Foto1}
+                        height='300'
+                        width='250'
+                        alt="Foto"
+                        className={styles.albumFoto}
+                    />
+                </div>
 
+                <div className={styles.albumFotoBoxIndividual}>
+                    <span>Bebendo 🍺</span>
+                    <Image
+                        src={Foto2}
+                        height='300'
+                        width='250'
+                        alt="Foto"
+                        className={styles.albumFoto}
+                    />
+                </div>
 
-                <Image
-                    src={Foto2}
-                    height='300'
-                    width='250'
-                    alt="Foto"
-                    className={styles.albumFoto}
-                />
+                <div className={styles.albumFotoBoxIndividual}>
+                    <span>Na melhor companhia</span>
+                    <Image
+                        src={Foto3}
+                        height='300'
+                        width='250'
+                        alt="Foto"
+                        className={styles.albumFoto}
+                    />
+                </div>
 
-                <Image
-                    src={Foto3}
-                    height='300'
-                    width='250'
-                    alt="Foto"
-                    className={styles.albumFoto}
-                />
-
-                <Image
-                    src={Foto4}
-                    height='250'
-                    width='350'
-                    alt="Foto"
-                    className={styles.albumFoto}
-                />
+                <div className={styles.albumFotoBoxIndividual}>
+                    <span>Praia 🏖️</span>
+                    <Image
+                        src={Foto4}
+                        height='250'
+                        width='350'
+                        alt="Foto"
+                        className={styles.albumFoto}
+                    />
+                </div>
 
             </div>
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import styles from './styles/app.module.css'
-import foto1 from '../../public/assets/images/01.jpeg'
+import foto from '../../public/assets/images/03.jpeg'
 import { Button } from 'antd'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.app} style={{ backgroundImage: `url('${foto1.src}')`, width: '100%', height: '100%', backgroundRepeat: 'round' }}>
+    <div className={styles.app} style={{ backgroundImage: `url('${foto.src}')`, width: '100%', height: '100%', backgroundRepeat: 'round' }}>
      
       <Link href='/album'>
       <Button>Album</Button>
