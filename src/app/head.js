a05661adc29e4c4cc666fb1fa038f9c78d62556a
Thales Neves <thales.neves@sap.ce.gov.br>
2024-06-06 20:16:15 -0300

    add ico foto

--- src/app/head.js
+++ src/app/head.js
@@ -7,7 +7,7 @@ export default function Head() {
             <head>
                 <title>Tamires e Thales</title>
                 <link rel="icon" href="/assets/icons/favicon.ico" />
-                <link rel="apple-touch-icon" href="/assets/icons/Icon.jpeg"></link>
+                <link rel="apple-touch-icon" href="/assets/icons/favicon.ico"></link>
                 <link rel="manifest" href="/manifest.json" />
                 <meta name="description" content="Página especial para Tamires e Thales" />
                 <meta name="theme-color" content="#000" />
