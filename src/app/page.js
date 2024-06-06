bb7d6885120194e2915a586d93e46b5a211516e8
Thales Neves <thales.neves@sap.ce.gov.br>
2024-06-05 23:06:28 -0300

    remove texto home

--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,9 @@ export default function Home() {
 
       <div style={{ backgroundColor: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: 10, margin: 20, borderRadius: 10 }}>
 
-        Parabéns amor, ja está na reta final..  
-        Só para você não esquecer EU TE AMO!! ❤️
+        EU TE AMO!! ❤️
         <br/>
-        <br/>
-        <span style={{fontWeight: 'bold'}}>Vamos ver se é esperta mesmo, vá para o local mais quente da casa quando ligado</span>
+
       </div>
      
       {/*<Image src={foto1}></Image>*/}
