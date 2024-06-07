75e2cb80d96c7dc122c03c7b774bd94aca225a56
Thales Neves <thales.neves@sap.ce.gov.br>
2024-06-07 12:07:57 -0300

    teste whats

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,52 @@
+'use client'
+
 import Image from 'next/image'
 import styles from '@/styles/app.module.css'
 import foto from '../../public/assets/images/foto.jpeg'
-import { Button } from 'antd'
+import { Button, message } from 'antd'
 import Link from 'next/link'
+import axios from 'axios'
+import { useState } from 'react'
 
 export default function Home() {
+
+  //Obj whats
+  const whats = {
+    chatId: `558898550669@c.us`,
+    contentType: "string",
+    content: "Eu te amo muito e estou com saudade!"
+  }
+
+  const [objWhats, setObjWhats] = useState(whats)
+
+  //Enviat whats
+  const EnviarWhats = () => {
+    axios.post("https://whats.thbytes.com.br/client/sendMessage/comunidadezdg", {
+      ...objWhats
+    }).then((response) => {
+      message.success("Whats enviado com sucesso ", 5)
+    })
+      .catch(error => {
+        if (error.message === "Network Error") {
+          message.error("Erro de conexão, verifique sua internet ou tente novamente", 5)
+        } else {
+          message.error("Erro ao enviar", 5)
+        }
+      })
+  }
+
+
   return (
     <div className={styles.app} style={{ backgroundImage: `url('${foto.src}')`, width: '100%', height: '100%', backgroundRepeat: 'round', marginTop: 50 }}>
-
+      
       <div style={{ backgroundColor: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: 10, margin: 20, borderRadius: 10 }}>
 
         EU TE AMO!!!! ❤️
-        <br/>
+        <Button onClick={() => EnviarWhats()} type='primary'>Clique aqui</Button>
+        <br />
 
       </div>
-     
+
       {/*<Image src={foto1}></Image>*/}
 
     </div>
